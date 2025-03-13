4792af0d2cd5024b7be3c60e4485cb5c1ad4d3bd
mick <mick_bythell@hotmail.com>
2025-03-13 13:23:34 +0100

    removed cond comp code

--- sim/shared/video_hardware.h
+++ sim/shared/video_hardware.h
@@ -2,36 +2,21 @@
 
 namespace VideoHardware {
 
-#if !defined(VIDEO_HARDWARE_NO_BUFFER)
 typedef struct { // for SDL texture
   uint8_t a;     // transparency
   uint8_t b;     // blue
   uint8_t g;     // green
   uint8_t r;     // red
 } Pixel;
-#else
-typedef struct {
-  uint8_t a; // transparency
-  uint8_t b; // blue
-  uint8_t g; // green
-  uint8_t r; // red
-  int sx, sy;
-  bool video_enable;
-  bool frame_start;
-} VideoData;
-#endif
 
 // hardware currently restricted to 640x480
 const unsigned int HRES=640;
 const unsigned int VRES=480;
 
 struct VideoHardware {
-#if !defined(VIDEO_HARDWARE_NO_BUFFER)
   Pixel screenbuffer[HRES * VRES];
-#endif
   Vsim hw;
 
-#if !defined(VIDEO_HARDWARE_NO_BUFFER)
   void cycle() {
     hw.video_clk_pix = 1;
     hw.eval();
@@ -47,17 +32,6 @@ struct VideoHardware {
       p->r = hw.red;
     }
   }
-#else
-  VideoData cycle() {
-    hw.video_clk_pix = 1;
-    hw.eval();
-    hw.video_clk_pix = 0;
-    hw.eval();
-
-    return {0xFF,  hw.blue, hw.green,        hw.red,
-            hw.sx, hw.sy,   hw.video_enable, hw.frame_start};
-  }
-#endif
 
   ~VideoHardware() { hw.final(); }
 
